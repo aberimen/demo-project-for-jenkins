e9d2ed2d876a46f9f6d57086ee3d8642e96e748e
aberimen <berimen63@gmail.com>
2021-06-24 15:49:59 +0300

    Initial commit

---
+++ src/main/java/com/aberimen/inheritance/DemoProjectForJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.aberimen.inheritance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectForJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectForJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aberimen/inheritance/DemoProjectForJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aberimen.inheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectForJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
